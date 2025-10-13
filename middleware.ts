00cf4d322baa692e8b8242f6c425a49e991c471b
Shompa <nico_nahuelpan@hotmail.com>
2025-10-13 01:55:50 -0300

    fix:[dev] Reduce middleware size

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
-// NO importes nada de "@/auth"
-export { auth as middleware } from "next-auth";
+export { auth as middleware } from "@/auth"
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
-};
+}
